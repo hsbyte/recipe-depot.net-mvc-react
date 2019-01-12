b9bd5fad30fc7ea6223e5464e033751db9e9ffd6
Arnold F. Haban <36382588+hsbyte@users.noreply.github.com>
2019-01-11 23:18:22 -0600

    Add file

---
+++ RecipeDepot/Controller/VideoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace RecipeDepot.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class VideoController : ControllerBase
+	{
+		//[HttpPost("{id}")]
+		//public async Task<IActionResult> UploadVideo([FromRoute] string id, IFormFile file)
+		//{
+		//		return BadRequest();
+		//}
+
+		// DELETE: api/image/delete/{filefull path -> id}
+		[HttpDelete("delete/{id}")]
+		public IActionResult DeleteVideo([FromRoute] string id)
+		{
+			return Ok(new { status = "File deleted." });
+		}
+	}
+}
